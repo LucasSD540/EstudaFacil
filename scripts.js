5be4c9966b66dac71cfaaea7ec25db2c68683532
Lucas Souza Duarte <lucas.souzaduarte.73@gmail.com>
2024-09-27 12:22:30 -0300

    feat: update layout

--- scripts.js
+++ scripts.js
@@ -1,6 +1,7 @@
 const menu = document.getElementById("menuBtn");
 const navbarSmall = document.getElementById("nav-links");
 const closeBtn = document.getElementById("closeBtn");
+const allLinks = document.querySelectorAll("a");
 
 menu.addEventListener("click", function () {
   navbarSmall.classList.add("is-visible");
@@ -10,4 +11,11 @@ menu.addEventListener("click", function () {
 closeBtn.addEventListener("click", function () {
   navbarSmall.classList.remove("is-visible");
   closeBtn.classList.add("is-visible");
+});
+
+allLinks.forEach((link) => {
+  link.addEventListener("click", function () {
+    navbarSmall.classList.remove("is-visible");
+    closeBtn.classList.add("is-visible");
+  });
 });
